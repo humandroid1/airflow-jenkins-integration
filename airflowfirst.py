7b0c35c717262976fe4bd9620e3002dc93d45525
vishnu2198 <48847605+vishnu2198@users.noreply.github.com>
2020-06-29 21:08:31 +0530

    Create airflowfirst.py

---
+++ airflowfirst.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+#from airflow.operators import MultiplyBy5Operator
+
+def print_hello():
+ return 'Hello Wolrd'
+
+dag = DAG('vishnu_DAG_1', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
+dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
+hello_operator = PythonOperator(task_id='first_task', python_callable=print_hello, dag=dag)
+dummy_operator >> hello_operator
