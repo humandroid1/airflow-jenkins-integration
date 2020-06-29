558716b84f68a2d812b946b1744435372bafe292
vishnu2198 <48847605+vishnu2198@users.noreply.github.com>
2020-06-29 23:58:16 +0530

    Update and rename airflowfirst.py to airflowsecond.py

--- airflowsecond.py
+++ airflowsecond.py
@@ -7,7 +7,7 @@ from airflow.operators.python_operator import PythonOperator
 def print_hello():
  return 'Hello Wolrd'
 
-dag = DAG('vishnu_DAG_1', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
+dag = DAG('vishnu_DAG_2', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
 dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
-hello_operator = PythonOperator(task_id='first_task', python_callable=print_hello, dag=dag)
+hello_operator = PythonOperator(task_id='second_task', python_callable=print_hello, dag=dag)
 dummy_operator >> hello_operator
